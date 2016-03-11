5c0ceb2244b4f48b02951b6fa0ab71f068e2a2c6
JeffChiu <jchiu@JeffChius-MacBook-Pro.local>
2016-03-10 23:42:08 -0800

    solution

--- swift-conjunction-function/AppDelegate.swift
+++ swift-conjunction-function/AppDelegate.swift
@@ -1,24 +1,20 @@
 //  AppDelegate.swift
-
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
         /*
-
         * Call your function here!
-
         */
         
         // do not alter
         return true //
     }   /////////////
 }       ////////////
-
