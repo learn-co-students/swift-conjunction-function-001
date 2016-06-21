39ee2e9fccf35c0f3c5b55bafaff6da6e6a52738
John Richardson <richardsonjm@gmail.com>
2016-06-20 23:27:54 -0400

    List with elements

--- swift-conjunction-function/Sandbox.swift
+++ swift-conjunction-function/Sandbox.swift
@@ -2,9 +2,25 @@
 
 import Foundation
 
-/*
-
-* Write your function here!
-
-*/
-
+func listWithElements(elements: [String], conjunction: String) -> String {
+    let acceptableConjunctions = ["and", "or", "&"]
+    if !acceptableConjunctions.contains(conjunction) {
+        return "unknown conjunction"
+    }
+    switch elements.count {
+    case 0:
+        return ""
+    case 1:
+        return elements[0]
+    case 2:
+        return elements[0] + " \(conjunction) " + elements[1]
+    default:
+        var list = "\(elements[0]), "
+        let indexOfLastElement = elements.count - 1
+        for i in 1 ..< indexOfLastElement {
+            list = list + "\(elements[i]), "
+        }
+        list = "\(list)\(conjunction) \(elements[indexOfLastElement])"
+        return list
+    }
+}
